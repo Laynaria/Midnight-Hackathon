a6044e3da6dbb96619b0d2db08023e95c1b3d197
Elie Parthenay <elie.parthenay@gmail.com>
2023-01-11 17:20:09 +0100

    install fontawesome and create header component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
+import Header from "../components/layout/Header";
+
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline text-orange-600">Accueil</h1>
-  );
+  return <Header />;
 }
