54700f581b348bc7350a9943fe433054d0f3a895
mlgs <mathieu.langlois@voxm.live>
2023-01-11 19:33:55 +0100

    table content add

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -7,6 +7,46 @@ export default function Dashboard() {
       <LeftBar elemActive="" />
       <main>
         <Header />
+        <div className="adminContent">
+          <div className="kpiContent">
+            <div className="kpiContent1">
+              <div className="kpiContent1Title">Value</div>
+              <div className="kpiContent1Value">45 000€</div>
+            </div>
+            <div className="kpiContent2">
+              <div className="kpiContent1Title">Value</div>
+              <div className="kpiContent1Value">45 000€</div>
+            </div>
+            <div className="kpiContent3">
+              <div className="kpiContent1Title">Value</div>
+              <div className="kpiContent1Value">45 000€</div>
+            </div>
+            <div className="kpiContent4">
+              <div className="kpiContent1Title">Value</div>
+              <div className="kpiContent1Value">45 000€</div>
+            </div>
+          </div>
+          <div className="content">
+            <div className="tableItem">
+              <div className="tableTitle">Vehicles</div>
+              <div className="tableContent" />
+            </div>
+            <div className="tableItem">
+              <div className="tableTitle">Maintenance scheduled</div>
+              <div className="tableContent" />
+            </div>
+          </div>
+          <div className="content">
+            <div className="tableItem">
+              <div className="tableTitle">Bookings</div>
+              <div className="tableContent" />
+            </div>
+            <div className="tableItem">
+              <div className="tableTitle">Users</div>
+              <div className="tableContent" />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
