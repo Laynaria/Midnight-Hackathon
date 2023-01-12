9bacc8d51715f19a4ebf9addd0b90649ea1c8a67
saigyou_ayakashi <minuit1993@live.fr>
2023-01-12 09:16:10 +0100

    fixe name

--- backend/src/routes/fileRouter.js
+++ backend/src/routes/fileRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const multer = require("multer");
 
-const upload = multer({ dest: "uploads/d" });
+const upload = multer({ dest: "uploads/" });
 const getFile = require("../controllers/fileController");
 
 router.post("/file", upload.single("img"), getFile);
